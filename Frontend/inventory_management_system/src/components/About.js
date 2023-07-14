7efede3d47c24bc7fd4daf7846714acbb5be60a6
Hammad <mhy20401@gmail.com>
2023-07-14 12:02:05 +0500

    Created About Component

---
+++ Frontend/inventory_management_system/src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='container-fluid p-5'>
+        <h1>Inventory Management System - MERN CRUD App</h1>
+        <p>...</p>
+    </div>
+  )
+}
