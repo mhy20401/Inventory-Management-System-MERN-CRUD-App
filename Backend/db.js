5ec8c52136d16e5854576dd5110f8d7ee4df4107
Hammad <mhy20401@gmail.com>
2023-07-14 11:41:36 +0500

    Connected to MongoDB Compass
    - Database name is IMS
    - Collection name is products

---
+++ Backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const mongoURI = "mongodb://127.0.0.1:27017/IMS";
+
+const connectToMongo = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(mongoURI);
+    console.log("Connected to Mongo Successfully!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = connectToMongo;
