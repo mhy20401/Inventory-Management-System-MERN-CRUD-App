09de09e2ca396c341ddf5bcc07d32aa41bdfd872
Hammad <mhy20401@gmail.com>
2023-07-14 11:52:37 +0500

    Created CRUD Operations on Products
    - POST (Create / insert)
    - GET (read / find)
    - PUT (Update)
    - Delete

---
+++ Backend/Routes/router.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const router = express.Router();
+const products = require('../Models/Products');
+
+//Inserting(Creating) Data:
+router.post("/insertproduct", async (req, res) => {
+    const { ProductName, ProductPrice, ProductBarcode } = req.body;
+
+    try {
+        const pre = await products.findOne({ ProductBarcode: ProductBarcode })
+        console.log(pre);
+
+        if (pre) {
+            res.status(422).json("Product is already added.")
+        }
+        else {
+            const addProduct = new products({ ProductName, ProductPrice, ProductBarcode })
+
+            await addProduct.save();
+            res.status(201).json(addProduct)
+            console.log(addProduct)
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+//Getting(Reading) Data:
+router.get('/products', async (req, res) => {
+
+    try {
+        const getProducts = await products.find({})
+        console.log(getProducts);
+        res.status(201).json(getProducts);
+    }
+    catch (err) {
+        console.log(err);
+    }
+})
+
+//Getting(Reading) individual Data:
+router.get('/products/:id', async (req, res) => {
+
+    try {
+        const getProduct = await products.findById(req.params.id);
+        console.log(getProduct);
+        res.status(201).json(getProduct);
+    }
+    catch (err) {
+        console.log(err);
+    }
+})
+
+//Editing(Updating) Data:
+router.put('/updateproduct/:id', async (req, res) => {
+    const { ProductName, ProductPrice, ProductBarcode } = req.body;
+
+    try {
+        const updateProducts = await products.findByIdAndUpdate(req.params.id, { ProductName, ProductPrice, ProductBarcode }, { new: true });
+        console.log("Data Updated");
+        res.status(201).json(updateProducts);
+    }
+    catch (err) {
+        console.log(err);
+    }
+})
+
+//Deleting Data:
+router.delete('/deleteproduct/:id', async (req, res) => {
+
+    try {
+        const deleteProduct = await products.findByIdAndDelete(req.params.id);
+        console.log("Data Deleted");
+        res.status(201).json(deleteProduct);
+    }
+    catch (err) {
+        console.log(err);
+    }
+})
+
+
+module.exports = router;
