7fed6f6a96b0486d7b36553c08274e3aad588b46
Hammad <mhy20401@gmail.com>
2023-07-14 11:39:29 +0500

    Created Schema for Products
    - Database name is IMS in MongoDB
    - Collection name is products in MongoDB

---
+++ Backend/Models/Products.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const ProductSchema = new mongoose.Schema(
+    {
+        ProductName: {
+            type: String,
+            required: true,
+        },
+        ProductPrice: {
+            type: Number,
+            required: true,
+        },
+        ProductBarcode: {
+            type: Number,
+            required: true,
+        },
+    });
+
+const Products = mongoose.model("Products", ProductSchema)
+module.exports = Products;
